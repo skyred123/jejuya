81b8941f4203a37ac058f661f9c0188db3e413ba
NQ556 <116347206+NQ556@users.noreply.github.com>
2024-11-27 09:19:06 +0700

    Remove duplicated code

--- lib/app/layers/data/sources/network/app_api_service.dart
+++ lib/app/layers/data/sources/network/app_api_service.dart
@@ -63,8 +63,6 @@ abstract class AppApiService extends BaseApiService {
     String? endDate,
     List<ScheduleItem>? listDestination,
   });
-
-  Future<List<Hotel>> fetchHotels();
   
   Future<UserDetail> fetchUserDetail();
 
@@ -299,21 +297,7 @@ class AppApiServiceImpl extends AppApiService {
     );
   }
 
-  @override
-  Future<List<Hotel>> fetchHotels() {
-    return performGet(
-      'hotel/all',
-      decoder: (data) {
-        // Check if the data is a Map and contains the 'data' key
-        if (data is Map && data['data'] is List) {
-          // Map the 'data' list into Destination objects
-          return (data['data'] as List)
-              .map((item) => Hotel.fromJson(item as Map<String, dynamic>))
-              .toList();
-        } else {
-          throw Exception('Unexpected response format: data is not a list.');
-        }
-        
+  @override        
   Future<UserDetail> fetchUserDetail() async {
     String? token =
         "${await fba.FirebaseAuth.instance.currentUser?.getIdToken()}";
